b8b8fef2cecf3f624258d14b8bdc144260a0480e
Caio Assis <assis.caio@outlook.com.br>
2021-10-29 18:29:19 -0300

    Commit inicial

---
+++ nix/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "assis";
+	home.homeDirectory = "/home/assis/";
+	programs.home-manager.enable = true;
+	home.stateVersion="21.05";
+	home.packages = with pkgs; [
+	#	nodejs-16_x
+		yarn
+		vscode
+	];
+	home.sessionVariables = {
+		EDITOR = "vim";
+	};
+
+	programs.vim = {
+		enable = true;
+		settings = {
+			number = true;
+		};
+	};
+}
+
