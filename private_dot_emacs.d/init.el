a77b9ad7156ceaf0b06d2c72d6b663c69fbb9535
Caio Assis <assis.caio@outlook.com.br>
2025-03-08 21:29:22 -0300

    Commit inicial

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,15 @@
+(set-register ?e (cons 'file user-init-file))
+(setq user-mail-address "cassis.santos@outlook.com")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(elixir-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
