85b88a53c6d3ebbb311dadb64c475cccc308e631
Caio Assis Souza Santos <assis.caio@outlook.com.br>
2025-03-14 15:33:23 -0300

    Commit Inicial

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,18 @@
+;; Registers
+(set-register ?e (cons 'file user-init-file))
+(set-register ?h (cons 'file "~/.config/hypr/hyprland.conf"))
+
+;;(setq user-mail-address )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(elixir-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
