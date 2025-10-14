ba280c90f9d179d1f2a33b04089d304b3d751e5c
Caio Assis Souza Santos <cassis.santos@pm.me>
2025-10-14 16:12:11 -0300

    Ajustes no gerenciamento de pacotes

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,23 +1,44 @@
-;; Registers
-(setq inhibit-startup-message t)
+;; Variaveis
+(global-set-key (kbd "C-c h") 'hippie-expand)
 
+(setq inhibit-startup-message t)
+;;(setq tab-stop-list 'nil)
+;;
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (load-theme 'leuven t)
 ;; Tamanho da fonte
 (set-face-attribute 'default nil :family "JetBrains Mono" :height 110)
+;; Registers
 (set-register ?e (cons 'file user-init-file))
 (set-register ?h (cons 'file "~/.config/hypr/hyprland.conf"))
-(load-theme 'leuven)
-(require 'package)
-(setq package-enable-at-startup nil)
 
+;; Configuração de pacotes
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/")
 	     )
-	     
+(require 'package)
+(setq package-enable-at-startup nil)
+;;(setq use-package-always-ensure t)
 (package-initialize)
+;;(package-refresh-contents)
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
+
+
+(load-theme 'leuven)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2d74de1cc32d00b20b347f2d0037b945a4158004f99877630afc034a674e3ab7"
+     default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
