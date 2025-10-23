b3b6f80c7ce9ae9f421fe766bc007a30d407e92a
Caio Assis Souza Santos <cassis.santos@pm.me>
2025-10-23 09:12:41 -0300

    Disable do ring-bell. Adicao do projectile

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,10 +1,17 @@
 ;; Variaveis
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-c h") 'hippie-expand)
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+;;(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; Não exibir mensagem inicial
 (setq inhibit-startup-message t)
 
+;; Desabilitar ring-bell (feedback sonoro)
+(setq ring-bell-function 'ignore)
+;; Modes
 ;; Não exibir barra de menu
 (menu-bar-mode -1)
 
@@ -17,10 +24,14 @@
 ;; Modo de arquivos recentes
 (recentf-mode 1)
 
+;; Exibir números de linhas
+(display-line-numbers-mode t)
+
 (load-theme 'wheatgrass t)
 
 ;; Tamanho da fonte
-(set-face-attribute 'default nil :family "JetBrains Mono" :height 120)
+;;(set-face-attribute 'default nil :family "JetBrains Mono" :height 120)
+(set-face-attribute 'default nil :family "Consolas" :height 120)
 
 ;; Registers
 (set-register ?e (cons 'file user-init-file))
@@ -32,9 +43,8 @@
 	     )
 (require 'package)
 (setq package-enable-at-startup nil)
-;;(setq use-package-always-ensure t)
 (package-initialize)
-;;(package-refresh-contents)
+
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 ;; Pacote para exibir sugestoes no mini-buffer
@@ -43,16 +53,19 @@
   :config (which-key-mode))
 ;; Pacote para gerenciamento de arquivos e projetos
 (use-package projectile
-  :ensure t)
-(use-package paper-theme)
-;; Pacote para autocomplete geral
-;; (use-package auto-complete
-  ;; :ensure t
-  ;; :init
-  ;; (progn
-    ;; (ac-config-default)
-    ;; (global-auto-complete-mode t)))
+  :ensure t
+  :config (projectile-mode))
 
+;; framework para completion do mini-buffer
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
+;; Desenvolvimento de Software
+(use-package magit
+  :ensure t)
+;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -62,7 +75,7 @@
    '("da4227ca47b0f4f9bddffec324d6d40ecd97a5daaa6d1974020e7284a5f90c81"
      "2d74de1cc32d00b20b347f2d0037b945a4158004f99877630afc034a674e3ab7"
      default))
- '(package-selected-packages '(auto-complete projectile try which-key)))
+ '(package-selected-packages '(magit projectile try vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
